346e6eb56c9e312efc6f55af129e04fbb65eaaa3
wuyabing <wuyabing@findpiano.cn>
2020-11-19 16:19:00 +0800

    typescript

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import { createApp } from 'vue'
-// import Ant from 'ant-design-vue'
+import Ant from 'ant-design-vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-// import 'ant-design-vue/dist/antd.css'
-// import 'normalize.css'
+import 'ant-design-vue/dist/antd.css'
+import 'normalize.css'
 
 const app = createApp(App)
 
-// app.use(Ant)
+app.use(Ant)
 app.provide('store', store)
 app.use(router).mount('#app')
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,7 +1,7 @@
 import NotFount from '@/views/notFount.vue'
 import Page1 from '@/views/page1.vue'
 import Page2 from '@/views/page2.vue'
-import Page3 from '@/views/page3/page3.vue'
+import Page3 from '@/views/page3.vue'
 
 export default [
   {
